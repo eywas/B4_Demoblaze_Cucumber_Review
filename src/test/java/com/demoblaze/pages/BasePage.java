424e8edb58e1438a05c8e46607e2f37501251228
eywas <umiteywas@gmail.com>
2023-08-02 13:32:18 +0300

    01 agust 2023

--- src/test/java/com/demoblaze/pages/BasePage.java
+++ src/test/java/com/demoblaze/pages/BasePage.java
@@ -7,9 +7,15 @@ import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePage {
 
+
     public BasePage() {
         PageFactory.initElements(Driver.get(),this);
     }
+    @FindBy(xpath = "(//a[@class='nav-link'])[1]")
+    public WebElement homeModule;
+
+    @FindBy(xpath = "//a[.='Cart']")
+    public  WebElement cartModule;
 
     @FindBy(id = "nameofuser")
     public WebElement nameofuser;
--- src/test/java/com/demoblaze/pages/LoginPage.java
+++ src/test/java/com/demoblaze/pages/LoginPage.java
@@ -2,10 +2,13 @@ package com.demoblaze.pages;
 
 import com.demoblaze.utilities.BrowserUtils;
 import com.demoblaze.utilities.ConfigurationReader;
+import com.demoblaze.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
 
     @FindBy(id = "login2")
     public WebElement loginHomePage;
@@ -16,9 +19,9 @@ public class LoginPage extends BasePage{
     @FindBy(css = "[onclick='logIn()']")
     public WebElement submitBtn;
 
-    public void login(){
-        String username= ConfigurationReader.get("username");
-        String password= ConfigurationReader.get("password");
+    public void login() {
+        String username = ConfigurationReader.get("username");
+        String password = ConfigurationReader.get("password");
 
         loginHomePage.click();
         input_loginusername.sendKeys(username);
@@ -27,5 +30,22 @@ public class LoginPage extends BasePage{
         BrowserUtils.waitFor(2);
 
     }
+
+    public void login(String username, String password) {
+        loginHomePage.click();
+        input_loginusername.sendKeys(username);
+        input_loginpassword.sendKeys(password);
+        submitBtn.click();
+        BrowserUtils.waitFor(2);
+
+    }
+
+    public void verifyWithPopUpMessage(String expectedMessage){
+        Alert alert= Driver.get().switchTo().alert();
+        String actualMessage=alert.getText();
+        System.out.println("actualMessage = " + actualMessage);
+        Assert.assertEquals(expectedMessage,actualMessage);
+    }
+
 
 }
